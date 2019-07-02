8398a0c408180d5ce732212918192eb6ac626177
cdsalty <soltiscd@gmail.com>
2019-07-01 23:41:31 -0400

    added error for callbacks

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// will practice and refresh using ES5 and prototypes
+// instantiate http;
+const http = new easyHTTP();
+
+// GET POST
+http.get('https://jsonplaceholder.typicode.com/posts',
+function(err, post) {    // post will be whatever 'self.http.responseText' is equal to in simplehttp.js
+  console.log(posts);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://jsonplaceholder.typicode.com/
--- simplehttp.js
+++ simplehttp.js
@@ -1,29 +1,35 @@
+
 /*
 This is a refresh using ES5 and prototypes; will update later versions to fetch;
   - including practice with making request and prototypes
 */ 
 
 function easyHTTP(){
-  this.http = new XMLHttpRequest(); // setting http to the HTTP Request Object
+  this.http = new XMLHttpRequest(); // setting http to the HTTP Request Object (only property we have)
   // previously used 'xhr' = and also xhr.open(), etc.
 }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url){
+easyHTTP.prototype.get = function(url, callback) {
   this.http.open('GET', url, true);
 
-  this.http.onload = function(){
+  let self = this; // to capture 'this' scope
+  this.http.onload = function() {  // the ES6 arrow syntax would prevent this problem (this.http.onload = () => {)
     // check the status and make sure it's 200
-    if(this.http.status === 200) {
-       console.log(this.http.responseText);
+    if(self.http.status === 200) {
+       callback(null, self.http.responseText); // error will be the first thing passed back if there is one
+       // but we want to have an error shown if an error occurs; other than 200? 
+       // Do this with our else statement that's getting added in now.
+    } else {
+      callback(`Error: Possibly an id-ten-t ` + self.http.status); // if error, such as 404
     }
   }
 
   this.http.send();
-
+  
 }
 
-
+// inside a function, the 'this' keyword belongs to that function. 8:55
 
 // Make an HTTP POST Request
 
