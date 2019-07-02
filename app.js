08edfe65516f21c5d24f88ee09c6768b30cd8e0f
cdsalty <soltiscd@gmail.com>
2019-07-01 23:43:37 -0400

    added return err for get post error

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const http = new easyHTTP();
 // GET POST
 http.get('https://jsonplaceholder.typicode.com/posts',
 function(err, post) {    // post will be whatever 'self.http.responseText' is equal to in simplehttp.js
+  if (err) {
+    console.log(err);
+  } else 
   console.log(posts);
 });
 
